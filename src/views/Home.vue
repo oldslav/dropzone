7d3f095d466747d3251a9100bccac042ae9dee72
Slava Afanasev <vyaches.afanas@gmail.com>
2019-03-22 12:13:08 +0400

    basic functions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,62 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <div id="dnd">
+      <form ref="fileform" class="dndform">
+        <span>Drag Here</span>
+      </form>
+    </div>
+    <div v-for="(file, key) in files" v-bind:key="key">
+      <p>{{ file.name }}</p>
+    </div>
+    <button @click="output">Click</button>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue, Provide } from 'vue-property-decorator';
 
 @Component({
-  components: {
-    HelloWorld,
-  },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  @Provide() public files: any = [];
+  @Provide() public dragAndDropCapable: any = false;
+
+  public $refs!: {
+    fileform: HTMLFormElement,
+  };
+
+  public output() {
+    console.log(this.files);
+  }
+
+  public determineDragAndDropCapable() {
+    const div = document.createElement('div');
+    return (('draggable' in div)
+      || ('ondragstart' in div && 'ondrop' in div))
+      && 'FormData' in window
+      && 'FileReader' in window;
+  }
+
+  public preventDefaults(e: any) {
+    e.stopPropagation();
+    e.preventDefault();
+  }
+
+  public mounted() {
+    this.dragAndDropCapable = this.determineDragAndDropCapable;
+
+    if (this.dragAndDropCapable) {
+      ['drag', 'dragstart', 'dragend', 'dragover', 'dragenter', 'dragleave', 'drop']
+        .forEach((evt) => {
+          this.$refs.fileform.addEventListener(evt, this.preventDefaults, false);
+        });
+
+      this.$refs.fileform.addEventListener('drop', (e: any) => {
+        for (let i = 0; i < e.dataTransfer.files.length; i++) {
+          this.files.push(e.dataTransfer.files[i]);
+        }
+      });
+    }
+  }
+}
 </script>
