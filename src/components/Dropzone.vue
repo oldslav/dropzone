438da69b760cb84485a081d76fbba65734024e38
Slava Afanasev <vyaches.afanas@yandex.ru>
2019-03-22 18:10:33 +0400

    dropzone redone

--- src/components/Dropzone.vue
+++ src/components/Dropzone.vue
@@ -1,54 +1,27 @@
 <template>
-  <div id="dnd">
-    <form ref="fileform" class="dndform">
-      <span>Drag Here</span>
-    </form>
-  </div>
+	<div id="dnd" class="dndform"
+	     @drop="onDrop"
+	     @dragover.prevent>
+		<span>Drag Here</span>
+	</div>
 </template>
 <script lang="ts">
-import { Vue, Component, Provide } from 'vue-property-decorator';
-import { Mutation } from 'vuex-class';
+    import {Vue, Component, Provide} from 'vue-property-decorator';
+    import {Mutation} from 'vuex-class';
 
-export default class Dropzone extends Vue {
+    @Component
+    export default class Dropzone extends Vue {
+        @Mutation('addFile')
+        public appendFile: any;
 
-  @Provide() public dragAndDropCapable: any = false;
-
-  @Mutation('addFile')
-  public appendFile: any;
-
-  public $refs!: {
-    fileform: HTMLFormElement,
-  };
-
-  public determineDragAndDropCapable() {
-    const div = document.createElement('div');
-    return (('draggable' in div)
-      || ('ondragstart' in div && 'ondrop' in div))
-      && 'FormData' in window
-      && 'FileReader' in window;
-  }
-
-  public preventDefaults(e: any) {
-    e.stopPropagation();
-    e.preventDefault();
-  }
-
-  public mounted() {
-    this.dragAndDropCapable = this.determineDragAndDropCapable;
-
-    if (this.dragAndDropCapable) {
-      ['drag', 'dragstart', 'dragend', 'dragover', 'dragenter', 'dragleave', 'drop']
-        .forEach((evt) => {
-          this.$refs.fileform.addEventListener(evt, this.preventDefaults, false);
-        });
-
-      this.$refs.fileform.addEventListener('drop', (e: any) => {
-        for (let i = 0; i < e.dataTransfer.files.length; i++) {
-          this.appendFile(e.dataTransfer.files[i]);
+        public onDrop(e: any) {
+            e.preventDefault();
+            e.stopPropagation();
+            for (let i = 0; i < e.dataTransfer.files.length; i++) {
+                this.appendFile(e.dataTransfer.files[i]);
+            }
         }
-      });
+
     }
-  }
-}
 </script>
         
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,67 +1,24 @@
 <template>
-  <div>
-    <div id="dnd">
-      <form ref="fileform" class="dndform">
-        <span>Drag Here</span>
-      </form>
-    </div>
-    <!-- <dropzone-component></dropzone-component> -->
-    <div v-for="(file, key) in fileState.files" v-bind:key="key">
-      <p>{{ file.name }}</p>
-    </div>
-  </div>
+	<div>
+		<dropzone-component></dropzone-component>
+		<div v-for="(file, key) in fileState.files" v-bind:key="key">
+			<p>{{ file.name }}</p>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Provide } from 'vue-property-decorator';
-import DropzoneComponent from '@/components/Dropzone.vue';
-import { State, Mutation } from 'vuex-class';
-@Component({
-  components: { DropzoneComponent },
-})
-export default class Home extends Vue {
-  // @Provide() public files: any = [];
+    import {Component, Vue, Provide} from 'vue-property-decorator';
+    import DropzoneComponent from '@/components/Dropzone.vue';
+    import {State, Mutation} from 'vuex-class';
 
-  @State('files')
-  public fileState!: any;
+    @Component({
+        components: {DropzoneComponent},
+    })
+    export default class Home extends Vue {
+        // @Provide() public files: any = [];
 
-  @Mutation('addFile')
-  public appendFile: any;
-
-  @Provide() public dragAndDropCapable: any = false;
-
-  public $refs!: {
-    fileform: HTMLFormElement,
-  };
-
-  public determineDragAndDropCapable() {
-    const div = document.createElement('div');
-    return (('draggable' in div)
-      || ('ondragstart' in div && 'ondrop' in div))
-      && 'FormData' in window
-      && 'FileReader' in window;
-  }
-
-  public preventDefaults(e: any) {
-    e.stopPropagation();
-    e.preventDefault();
-  }
-
-  public mounted() {
-    this.dragAndDropCapable = this.determineDragAndDropCapable;
-
-    if (this.dragAndDropCapable) {
-      ['drag', 'dragstart', 'dragend', 'dragover', 'dragenter', 'dragleave', 'drop']
-        .forEach((evt) => {
-          this.$refs.fileform.addEventListener(evt, this.preventDefaults, false);
-        });
-
-      this.$refs.fileform.addEventListener('drop', (e: any) => {
-        for (let i = 0; i < e.dataTransfer.files.length; i++) {
-          this.appendFile(e.dataTransfer.files[i]);
-        }
-      });
+        @State('files')
+        public fileState!: any;
     }
-  }
-}
 </script>
